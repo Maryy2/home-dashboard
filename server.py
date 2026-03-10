6afbb940ac93deb9bfac9c40058dcf596192e308
Maryy2 <maryprokupek20@gmail.com>
2026-03-10 20:01:40 +0100

    testing

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def home():
     data = {
         "cpu": psutil.cpu_percent(interval=1),
         "ram": psutil.virtual_memory().percent,
-        "disk": psutil.disk_usage('/').percent
+        "disk": psutil.disk_usage('/')
     }
     return render_template("index.html", data=data)
 
